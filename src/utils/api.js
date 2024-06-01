3000a1db644fccbf6af3eef751dcc4bb5d11d46e
Zishan <97729148+zishanAl@users.noreply.github.com>
2024-06-01 17:32:57 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-// const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: "bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTdhN2E5MTEyYWQ4MmExNjJkZTMwMTU0NmRlNGI2MCIsInN1YiI6IjY2NTgzYTNmMGUzNTg5YWQ1YzUwNThkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Po2aki6vzboyrCqWiHaF5crzA3gTv1rSgAqgwYoKxZY",
+    Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
